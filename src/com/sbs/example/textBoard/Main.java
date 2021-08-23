a103a1378fbfc47c7e293cbe8e0df60049d4aeed
freestyle385 <freestyle385@gmail.com>
2021-08-23 16:04:33 +0900

    게시물 상세조회 기능 구현(DB로부터 불러오기)_0823

--- src/com/sbs/example/textBoard/Main.java
+++ src/com/sbs/example/textBoard/Main.java
@@ -192,7 +192,75 @@ public class Main {
 					}
 				}
 				System.out.printf("%d번 게시글 수정이 완료되었습니다.\n", id);
-				
+
+			} else if (command.startsWith("article detail ")) {
+				int id = Integer.parseInt(command.split(" ")[2]);
+				System.out.printf("---%d번 게시글 상세정보---\n", id);
+				Article foundArticle = null;
+
+				Connection conn = null;
+				PreparedStatement pstat = null;
+				ResultSet rs = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "SELECT * FROM article WHERE id = " + id + ";";
+
+					pstat = conn.prepareStatement(sql);
+					rs = pstat.executeQuery();
+					
+					while (rs.next()) {
+						String regDate = rs.getString("regDate");
+						String updateDate = rs.getString("updateDate");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+
+						foundArticle = new Article(id, regDate, updateDate, title, body);
+					}
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (rs != null && !rs.isClosed()) {
+							rs.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstat != null && !pstat.isClosed()) {
+							pstat.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.println("게시물이 존재하지 않습니다.");
+					continue;
+				}
+
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("생성일 : %s\n", foundArticle.regDate);
+				System.out.printf("수정일 : %s\n", foundArticle.updateDate);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+
 			} else {
 				System.out.println("잘못된 명령어입니다.");
 			}
